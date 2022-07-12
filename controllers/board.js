26b1faf6f020f7f1faabc9a4213573a1944c4e42
codeMonkey-shin <sdw0711@hanmail.net>
2022-07-12 12:59:30 +0900

    FIX : 데이터베이스 리팩토링

--- controllers/board.js
+++ controllers/board.js
@@ -1,4 +1,4 @@
-const {board, user, comment} = require('../models');
+const {Post, User, Comment} = require('../models');
 
 const {isAuthorized} = require('../middleware/webToken')
 // const {mintToken} = require('./smartContract')
@@ -11,7 +11,7 @@ module.exports = {
             throw new CustomError("올바르지 않은 파라미터 값입니다.",StatusCodes.CONFLICT);
         }
         const decoded = await isAuthorized(req)
-        const userInfo = await user.findOne({
+        const userInfo = await User.findOne({
             where: {id: decoded.id},
         });
         const userId = userInfo.id
@@ -23,16 +23,16 @@ module.exports = {
         if (!title || !content) {
             throw new CustomError("올바르지 않은 파라미터 값입니다.", StatusCodes.CONFLICT);
         }
-        const newBoard = new board({
-            title,
-            content,
-            userId
+        const newPost = new Post({
+            title:title,
+            content:content,
+            user_id:userId
         });
         try {
             // db에 저장
-            const createdBoard = await newBoard.save();
+            const createdPost = await newPost.save();
             // await mintToken(userInfo.address, 5);
-            res.status(StatusCodes.CREATED).json({status: "success", data: {boardId: createdBoard.id}});
+            res.status(StatusCodes.CREATED).json({status: "success", data: {postId: createdPost.id}});
         } catch (err) {
             throw new Error(err);
         }
@@ -43,28 +43,28 @@ module.exports = {
             throw new CustomError("올바르지 않은 파라미터 값입니다.",StatusCodes.CONFLICT);
         }
         const decoded = await isAuthorized(req)
-        const userInfo = await user.findOne({
+        const userInfo = await User.findOne({
             where: {id: decoded.id},
         });
         const userId = userInfo.id
-        const boardData = await board.findOne({
+        const postData = await Post.findOne({
             where: {id: req.params.id},
         });
-        if (!boardData) {
+        if (!postData) {
             throw new CustomError(`글번호 ${req.params.id} 가 존재하지 않습니다.`,StatusCodes.CONFLICT);
         }
 
 
 
-        if(!boardData.userId===userId){
+        if(!postData.user_id===userId){
             throw new CustomError(`올바른 사용자가 아닙니다`,StatusCodes.CONFLICT);
         }
         const {title,content} = req.body;
-        await boardData.update({
+        await postData.update({
             title:title,
             content: content
         });
-        res.status(StatusCodes.OK).json({status: "success", data: {boardId: boardData.id}});
+        res.status(StatusCodes.OK).json({status: "success", data: {post_id: postData.id}});
     }),
 
     /*
@@ -72,28 +72,28 @@ module.exports = {
 */
     getWritingById: asyncWrapper(async (req, res) => {
         //전달받은 id를 가진 writing을 찾아옴
-        const boardData = await board.findOne({
+        const postData = await Post.findOne({
             where: {id: req.params.id},
         });
-        await board.increment({hit: 1}, {where: {id: req.params.id}})
+        await Post.increment({hit: 1}, {where: {id: req.params.id}})
         //해당 id를 가진 writing 없으면 에러 응답
-        if (!boardData) {
+        if (!postData) {
             throw new CustomError(`글번호 ${req.params.id} 가 존재하지 않습니다.`, StatusCodes.CONFLICT);
         }
-        const {id, title, content, hit, createdAt, updatedAt, userId} = boardData;
-        const {userName} = await user.findOne({
-            where: {id: boardData.userId},
+        const {id, title, content, hit, created_at, updated_at, user_id} = postData;
+        const {username} = await User.findOne({
+            where: {id: postData.user_id},
         });
         // db에 comments가 유저의 object id로 저장이 되어있기 때문에
         // 응답으로 보내줄 때는 유저의 id와 닉네임을 추가해서 보내준다.
-        const boardComments = await comment.findAll({
+        const postComments = await Comment.findAll({
             include: [
                 {
-                    model: user,
-                    attributes: ['userName']
+                    model: User,
+                    attributes: ['username']
                 }
             ],
-            where: {boardId: boardData.id},
+            where: {post_id: postData.id},
             });
 
         res.status(200).json({
@@ -103,36 +103,36 @@ module.exports = {
                 title,
                 content,
                 hit,
-                createdAt,
-                updatedAt,
-                userName,
-                boardComments,
+                created_at,
+                updated_at,
+                username,
+                postComments,
             },
         });
     }),
 
     getAllWriting: asyncWrapper(async (req, res) => {
-        const writings = await board.findAll();
+        const writings = await Post.findAll();
         // Array에 map을 돌 때 콜백함수가 비동기면 일반적인 방법으로는 구현이 안됨
         // 그래서 Promise.all을 사용함
         const data = await Promise.all(
             writings.map(
-                async ({id, title, createdAt, updatedAt, hit, userId}) => {
-                    const {userName} = await user.findOne({
-                        where: {id: userId},
+                async ({id, title, created_at, updated_at, hit, user_id}) => {
+                    const {username} = await User.findOne({
+                        where: {id: user_id},
                     });
-                    const comments = await comment.findAndCountAll({
-                        where: {boardId: id},
+                    const comments = await Comment.findAndCountAll({
+                        where: {post_id: id},
                     });
                     const commentsCount= comments.count
                     return {
                         id,
                         title,
-                        userName,
+                        username,
                         hit,
                         commentsCount,
-                        createdAt,
-                        updatedAt
+                        created_at,
+                        updated_at
                     };
                 }
             )
@@ -148,21 +148,18 @@ module.exports = {
             throw new CustomError("올바르지 않은 파라미터 값입니다.",StatusCodes.CONFLICT);
         }
         const {content} = req.body;
-        const boardData = await board.findOne({
+        const postData = await Post.findOne({
             where: {id: req.params.id},
         });
-        const boardId = boardData.id
+        const postId = postData.id
         const decoded = await isAuthorized(req)
-        const userInfo = await user.findOne({
-            where: {id: decoded.id},
-        });
-        if (!boardData) {
-            throw new CustomError(`글번호 ${boardId} 가 존재하지 않습니다.`,StatusCodes.CONFLICT);
+        if (!postData) {
+            throw new CustomError(`글번호 ${postId} 가 존재하지 않습니다.`,StatusCodes.CONFLICT);
         }
-        const newComment = new comment({
-            content,
-            userId: decoded.id,
-            boardId
+        const newComment = new Comment({
+            content:content,
+            user_id: decoded.id,
+            post_id:postId
         });
         try {
             // db에 저장
@@ -175,26 +172,26 @@ module.exports = {
     }),
     deleteToWriting : asyncWrapper(async (req, res) => {
 
-        const boardData = await board.findOne({
+        const postData = await Post.findOne({
             where: {id: req.params.id},
         });
-        const boardId = boardData.id
-        const boardUserId = boardData.userId
+        const postId = postData.id
+        const postUserId = postData.user_id
         const decoded = await isAuthorized(req)
-        const userInfo = await user.findOne({
+        const userInfo = await User.findOne({
             where: {id: decoded.id},
         });
-        if (!boardData) {
-            throw new CustomError(`글번호 ${boardId} 가 존재하지 않습니다.`,StatusCodes.CONFLICT);
+        if (!postData) {
+            throw new CustomError(`글번호 ${postId} 가 존재하지 않습니다.`,StatusCodes.CONFLICT);
         }
-        if (boardUserId!==userInfo.id) {
+        if (postUserId!==userInfo.id) {
             throw new CustomError(`게시글 작성자가 아닙니다.`,StatusCodes.CONFLICT);
         }
         try {
             // db에 저장
-            await  board.destroy(
+            await  Post.destroy(
                 {
-                    where: {id:boardId}
+                    where: {id:postId}
                 }
             )
             // await mintToken(userInfo.address, 1);
@@ -204,13 +201,13 @@ module.exports = {
         }
     }),
     deleteToComment : asyncWrapper(async (req, res) => {
-        const commentData = await board.findOne({
+        const commentData = await Post.findOne({
             where: {id: req.params.id},
         });
         const commentId = commentData.id
-        const commentUserId = commentData.userId
+        const commentUserId = commentData.user_id
         const decoded = await isAuthorized(req)
-        const userInfo = await user.findOne({
+        const userInfo = await User.findOne({
             where: {id: decoded.id},
         });
         if (!commentData) {
@@ -221,7 +218,7 @@ module.exports = {
         }
         try {
             // db에 저장
-            await  comment.destroy(
+            await  Comment.destroy(
                 {
                     where: {id:commentId}
                 }
