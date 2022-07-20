e1a65263873c744cf395e5c55e272df6a16a8b15
yooseungjun <hongildong6987@gmail.com>
2022-07-20 17:59:33 +0900

    asd

--- controllers/market.js
+++ controllers/market.js
@@ -37,11 +37,10 @@ module.exports = {
     }
 
     //item이 nft라는 뜻
-
     const parameters = [userAddress, tokenId, price];
     await nftBuy(parameters);
 
-    res
+    return res
       .status(102)
       .send({ message: "구매 요청이 완료되었습니다", data: final });
   }),
