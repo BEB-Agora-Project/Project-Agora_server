b628a207a42a699569d2d5900e97980fd3d55585
yooseungjun <hongildong6987@gmail.com>
2022-07-21 19:55:59 +0900

    전역변ㅅ ㅜ초기화

--- controllers/market.js
+++ controllers/market.js
@@ -56,9 +56,11 @@ module.exports = {
     //전역변수에서 nft구매가 성공했는지 실패했는지 subscriber를 통해 받아와 저장하고 그결과를 참조합니다.
     if (global.nft === "실패") {
       console.log("컨트롤러 NFT 구매 실패");
+      global.nft = "초기화";
       res.send("구매에 실패했습니다");
     } else if (global.nft === "성공") {
       console.log("컨트롤러 NFT 구매 성공");
+      global.nft = "초기화";
       res.send("구매에 성공했습니다");
     }
   }),
