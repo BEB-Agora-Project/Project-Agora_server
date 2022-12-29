276628a944c4461edb2da29c522c90b6cf6fbf97
nonon <nononcrust@gmail.com>
2022-12-30 00:26:44 +0900

    cors origin 변경

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // cors 에러를 잡아주기 위한 설정 -> 여기서는 로컬의 3000번 포트에대한 접근을 허용함
 app.use(
   cors({
-    origin: true,
+    origin: 'https://agora-psi.vercel.app/',
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
   })
